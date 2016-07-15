c205599da3fc34536a71c6c7a93e602ff751112d
ocramz <zocca.marco@gmail.com>
2016-07-15 12:24:43 +0200

    trying out Stack install from APT, Ubuntu 14

--- setup.sh
+++ setup.sh
@@ -3,8 +3,10 @@
 printf "\n=== APT-Installing dependencies\n"
 			     
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 575159689BEFB442
-# Ubuntu 16 :
-echo 'deb http://download.fpcomplete.com/ubuntu xenial main'| tee /etc/apt/sources.list.d/fpco.list
+# # Ubuntu 16 :
+# echo 'deb http://download.fpcomplete.com/ubuntu xenial main'| tee /etc/apt/sources.list.d/fpco.list
+# Ubuntu 14
+echo 'deb http://download.fpcomplete.com/ubuntu trusty main'|sudo tee /etc/apt/sources.list.d/fpco.list
 apt-get update && sudo apt-get install -y --no-install-recommends \
 		       stack build-essential gcc libgmp-dev
 
